c600198ad2e518527fac7b86f019d3c5ed40614b
alexbelz123 <belz.alexx@gmail.com>
2024-05-17 19:36:01 +0300

    connect wallet fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,12 @@ import { installGlobals } from '@remix-run/node';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths(), svgr()],
+  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths(), svgr(), nodePolyfills({exclude: ['fs','util'],})],
   resolve: {
     alias: {},
   },
