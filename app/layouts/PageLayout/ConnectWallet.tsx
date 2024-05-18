722e659b7e006ef0bee05a703f2f6ae9095bd823
alexbelz123 <belz.alexx@gmail.com>
2024-05-18 17:20:53 +0300

    connect btn address small update

--- app/layouts/PageLayout/ConnectWallet.tsx
+++ app/layouts/PageLayout/ConnectWallet.tsx
@@ -2,10 +2,17 @@ import { Button } from '~/atoms/Button';
 import { useUserContext } from '~/providers/UserProvider/user.provider';
 
 export const ConnectWallet = () => {
-  const { connect, userAddress } = useUserContext();
-  return (
+  const { connect, userAddress, signOut } = useUserContext();
+  return userAddress ? (
+    <div className="flex items-center gap-x-2">
+      <h5 className="text-body-xs text-green-main">{userAddress}</h5>
+      <Button variant="outline" size="outline" onClick={signOut}>
+        Disconnect wallet
+      </Button>
+    </div>
+  ) : (
     <Button variant="outline" size="outline" onClick={connect}>
-      {userAddress ? userAddress : 'Connect Wallet'}
+      Connect Wallet
     </Button>
   );
 };
