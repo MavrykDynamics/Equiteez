0bfd9571b7ee01334a34421dc1ebe6e7566ff047
alexbelz123 <belz.alexx@gmail.com>
2024-06-17 15:30:35 +0300

    footer styles update

--- app/layouts/PageLayout/Footer.tsx
+++ app/layouts/PageLayout/Footer.tsx
@@ -2,10 +2,10 @@ import clsx from 'clsx';
 import { Logo } from '../Logo';
 
 // consts
-import { SOCIALS } from 'app/consts/icons';
-import { Link } from '@remix-run/react';
+// import { SOCIALS } from 'app/consts/icons';
+// import { Link } from '@remix-run/react';
 
-import { FOOTER_LINKS } from './pagelayout.consts';
+// import { FOOTER_LINKS } from './pagelayout.consts';
 
 export const Footer = () => {
   return (
@@ -16,22 +16,22 @@ export const Footer = () => {
         <div className="max-w-[420px] flex flex-col items-start">
           <Logo />
           <p className="text-body text-content mt-[26px] mb-8">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare
-            cursus sed nunc eget dictum Sed ornare cursus sed nunc eget dictumd
-            nunc eget dictum Sed ornare cursus sed nunc eget dictum{' '}
+            The future of real estate ownership is digital. Start with just $50
+            to invest in fractionalized, fully compliant, income producing
+            assets worldwide.
           </p>
           <div className="flex items-center gap-x-[21px]">
-            {SOCIALS.map(({ Icon, id, url }) => (
+            {/* {SOCIALS.map(({ Icon, id, url }) => (
               <Link key={id} to={url}>
                 <div className="w-9 h-9 flex justify-center items-center">
                   <Icon />
                 </div>
               </Link>
-            ))}
+            ))} */}
           </div>
         </div>
         <div className="flex gap-x-[88px] pr-[63px]">
-          {FOOTER_LINKS.map(({ id, links, title }) => (
+          {/* {FOOTER_LINKS.map(({ id, links, title }) => (
             <div key={id} className="flex flex-col items-start gap-y-6">
               <h3 className="text-content text-card-headline">{title}</h3>
               {links.map((link, idx) => (
@@ -44,7 +44,7 @@ export const Footer = () => {
                 </Link>
               ))}
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
       <div className="bg-green-main h-10 mt-[72px] w-full" />
