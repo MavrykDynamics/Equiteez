0b2b87cb60335bada91b3c77340b220538ce5f3e
alexbelz123 <belz.alexx@gmail.com>
2024-08-27 10:35:54 +0300

    marketplace

--- app/layouts/Logo.tsx
+++ app/layouts/Logo.tsx
@@ -4,7 +4,7 @@ import LogoIcon from 'app/icons/logo.svg?react';
 export const Logo = () => {
   return (
     <Link to="/" className="flex items-center gap-x-[10px] w-auto">
-      <LogoIcon />
+      <LogoIcon className=" text-dark-green-500 stroke-current" />
       <span className="text-green-main text-[26px] font-semibold leading-6">
         EQUITEEZ
       </span>
--- app/layouts/PageLayout/Footer.tsx
+++ app/layouts/PageLayout/Footer.tsx
@@ -1,53 +1,54 @@
 import clsx from 'clsx';
-import { Logo } from '../Logo';
+import { FOOTER_LINKS } from './pagelayout.consts';
+import { Link } from '@remix-run/react';
+import { SOCIALS } from '~/consts/icons';
 
-// consts
-// import { SOCIALS } from 'app/consts/icons';
-// import { Link } from '@remix-run/react';
-
-// import { FOOTER_LINKS } from './pagelayout.consts';
+import LogoIcon from 'app/icons/logo.svg?react';
 
 export const Footer = () => {
   return (
-    <footer
-      className={clsx('bg-background-secondary flex flex-col items-center')}
-    >
-      <div className="flex justify-between px-[100px] pt-9 w-container">
+    <footer className={clsx('bg-sand-900 flex flex-col items-center')}>
+      <div className="flex justify-between px-[100px] pt-[54px] pb-[122px] w-container">
         <div className="max-w-[420px] flex flex-col items-start">
-          <Logo />
-          <p className="text-body text-content mt-[26px] mb-8">
+          <Link to="/" className="flex items-center gap-x-[10px] w-auto">
+            <LogoIcon className="text-white stroke-current" />
+            <span className="text-white text-[26px] font-semibold leading-6">
+              EQUITEEZ
+            </span>
+          </Link>
+          <p className="text-body text-white mt-[26px] mb-8">
             The future of real estate ownership is digital. Start with just $50
             to invest in fractionalized, fully compliant, income producing
             assets worldwide.
           </p>
           <div className="flex items-center gap-x-[21px]">
-            {/* {SOCIALS.map(({ Icon, id, url }) => (
+            {SOCIALS.map(({ Icon, id, url }) => (
               <Link key={id} to={url}>
                 <div className="w-9 h-9 flex justify-center items-center">
-                  <Icon />
+                  <Icon className="fill-white" />
                 </div>
               </Link>
-            ))} */}
+            ))}
           </div>
         </div>
         <div className="flex gap-x-[88px] pr-[63px]">
-          {/* {FOOTER_LINKS.map(({ id, links, title }) => (
+          {FOOTER_LINKS.map(({ id, links, title }) => (
             <div key={id} className="flex flex-col items-start gap-y-6">
-              <h3 className="text-content text-card-headline">{title}</h3>
+              <h3 className="text-white text-card-headline">{title}</h3>
               {links.map((link, idx) => (
                 <Link
                   key={link.label.concat(idx.toString())}
                   to={link.url}
-                  className="text-content text-body-xs"
+                  className="text-white text-body-xs"
                 >
                   {link.label}
                 </Link>
               ))}
             </div>
-          ))} */}
+          ))}
         </div>
       </div>
-      <div className="bg-green-main h-10 mt-[72px] w-full" />
+      {/* <div className="bg-green-main h-10 mt-[72px] w-full" /> */}
     </footer>
   );
 };
