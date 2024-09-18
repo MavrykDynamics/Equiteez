a236d61bc9aa5aa425f8e1cea3523e6502a95dd3
alexbelz123 <belz.alexx@gmail.com>
2024-09-18 09:14:01 +0300

    back rwas

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     'process.env': process.env,
   },
   build: {
-    minify: true,
+    minify: 'esbuild',
     cssMinify: true,
     ssr: true,
     rollupOptions: {
