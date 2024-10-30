455ea6bfd8a3ca65693ecaa166001ec0b18f8968
alexbelz123 <belz.alexx@gmail.com>
2024-10-30 17:37:47 +0200

    toasts styles

--- app/providers/ToasterProvider/toaster.provider.const.tsx
+++ app/providers/ToasterProvider/toaster.provider.const.tsx
@@ -1,17 +1,17 @@
 // Toaster statuses
-export const TOASTER_SUCCESS = 'success';
-export const TOASTER_ERROR = 'error';
-export const TOASTER_WARNING = 'warning';
-export const TOASTER_INFO = 'info';
-export const TOASTER_LOADING = 'loading';
+export const TOASTER_SUCCESS = "success";
+export const TOASTER_ERROR = "error";
+export const TOASTER_WARNING = "warning";
+export const TOASTER_INFO = "info";
+export const TOASTER_LOADING = "loading";
 
 // icon helper
 export const TOAST_ICON_MAPPER = {
-  [TOASTER_SUCCESS]: 'okay',
+  [TOASTER_SUCCESS]: "okay",
   [TOASTER_ERROR]: TOASTER_ERROR,
-  [TOASTER_WARNING]: 'error-triangle',
-  [TOASTER_INFO]: 'info-reg',
-  [TOASTER_LOADING]: TOASTER_LOADING,
+  [TOASTER_WARNING]: "attention",
+  [TOASTER_INFO]: "info-reg",
+  [TOASTER_LOADING]: "small-spinner",
 };
 
 // consts
@@ -20,25 +20,25 @@ export const ANIMATION_DURATION = 400;
 export const TOASTS_LIMIT = 5;
 
 // animations
-export const TOASTER_HIDE = 'hide';
-export const TOASTER_REVEAL = 'reveal';
+export const TOASTER_HIDE = "hide";
+export const TOASTER_REVEAL = "reveal";
 
 // COMMON TEXTS ACROSS LOADERS
 export const TOASTER_UPDATE_DATA_AFTER_ACTION_DATA = {
-  title: 'Processing',
-  message: 'Waiting for transaction confirmation...',
+  title: "Processing",
+  message: "Waiting for transaction confirmation...",
 };
 
-export const ACTION_START_MESSAGE_TEXT = 'Please wait 30s';
-export const ACTION_COMPLETION_MESSAGE_TEXT = 'All good :)';
-export const DEFAULT_REQUEST_COMPLETION_MESSAGE_TEXT = 'Request confirmed.';
+export const ACTION_START_MESSAGE_TEXT = "Please wait 30s";
+export const ACTION_COMPLETION_MESSAGE_TEXT = "All good :)";
+export const DEFAULT_REQUEST_COMPLETION_MESSAGE_TEXT = "Request confirmed.";
 
 // TOASTER TEXTS IDS
-export const TOASTER_SUBSCRIPTION_ERROR = 'TOASTER_SUBSCRIPTION_ERROR';
+export const TOASTER_SUBSCRIPTION_ERROR = "TOASTER_SUBSCRIPTION_ERROR";
 
 // Error page default texts
 
-export const errorHeaderDefaultText = 'This page is outside the universe';
+export const errorHeaderDefaultText = "This page is outside the universe";
 export const errorDescDefaultText = (
   <>
     The page you are trying to access doesn’t exist or has been moved.
@@ -48,14 +48,14 @@ export const errorDescDefaultText = (
 
 // with error in toaster context
 export const errorHeaderDefaultTextWhenError =
-  'Your spaceship has encountered a bug';
+  "Your spaceship has encountered a bug";
 export const errorDescDefaultTextWhenError = (
   <>
-    A significant error has been encountered, please take a look at our{' '}
+    A significant error has been encountered, please take a look at our{" "}
     <a href="https://docs.mavenfinance.io/" target="_blank" rel="noreferrer">
-      Docs{' '}
+      Docs{" "}
     </a>
-    or reach out to us through our{' '}
+    or reach out to us through our{" "}
     <a
       href="https://discord.com/invite/7VXPR4gkT6"
       target="_blank"
