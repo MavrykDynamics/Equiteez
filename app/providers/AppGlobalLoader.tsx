0abff9d8fadcde92f9f28ab66eb44bbb54544b1a
alexbelz123 <belz.alexx@gmail.com>
2024-12-10 15:20:15 +0200

    market ctz type

--- app/providers/AppGlobalLoader.tsx
+++ app/providers/AppGlobalLoader.tsx
@@ -1,13 +1,15 @@
-import { FC } from 'react';
-import { useWalletContext } from './WalletProvider/wallet.provider';
-import { Spinner } from '~/lib/atoms/Spinner/Spinner';
-import { useTokensContext } from './TokensProvider/tokens.provider';
+import { FC } from "react";
+import { useWalletContext } from "./WalletProvider/wallet.provider";
+import { Spinner } from "~/lib/atoms/Spinner/Spinner";
+import { useTokensContext } from "./TokensProvider/tokens.provider";
+import { useMarketProvider } from "./MarketProvider/market.provider";
 
 export const AppGlobalLoader: FC<PropsWithChildren> = ({ children }) => {
   const { isLoading } = useWalletContext();
+  const { isLoading: isMarketLoading } = useMarketProvider();
   const { isLoading: isTokensMetaLoading } = useTokensContext();
 
-  if (isLoading || isTokensMetaLoading)
+  if (isLoading || isTokensMetaLoading || isMarketLoading)
     return (
       <div className="h-screen w-full flex items-center justify-center bg-mvrk-dark">
         <Spinner size={56} />
