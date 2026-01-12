ae63fde9d42a8fd0d5c06a1fc70dbd20e0dcac0c
alexbelz123 <belz.alexx@gmail.com>
2026-01-12 16:01:27 +0200

    increased build chunks due to mocked estates data

--- vite.config.ts
+++ vite.config.ts
@@ -48,6 +48,7 @@ export default defineConfig({
       },
       workbox: {
         // caches your built client assets
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB
         globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest,woff2}"],
       },
       devOptions: {
