5fb6d01387b789c5964fa27299e817a1dc6740e9
alexbelz123 <belz.alexx@gmail.com>
2026-06-19 12:11:05 +0300

    updated basenet network

--- app/consts/rpcNodes.ts
+++ app/consts/rpcNodes.ts
@@ -1,10 +1,10 @@
-import { z } from 'zod';
+import { z } from "zod";
 // user can add the custom one from the settings popup
 
-export const atlasNetRpcnode = 'https://atlasnet.rpc.mavryk.network';
+export const basenetNetRpcnode = "https://basenet.rpc.mavryk.network";
 
-export const RPC_NODE = 'selectedRpcNode';
+export const RPC_NODE = "selectedRpcNode";
 
-export const rpcNodeSchema = z.enum([atlasNetRpcnode]);
+export const rpcNodeSchema = z.enum([basenetNetRpcnode]);
 
 export type RPCNodeType = z.infer<typeof rpcNodeSchema>;
