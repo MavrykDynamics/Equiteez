d78dc2fc211ce2b5486bd59667c17d6f5dff5cdc
AndriyPytsko <AserExtensa52@gmail.com>
2026-07-22 12:35:00 +0300

    MAV Add new Discover page

--- app/routes/discover/components/AssetsFilters/AssetsFilters.tsx
+++ app/routes/discover/components/AssetsFilters/AssetsFilters.tsx
@@ -3,15 +3,22 @@ import { Text } from "~/lib/atoms/Typography/Text";
 import { useAssetsContext } from "~/providers/AssetsProvider/assets.provider";
 import { useMemo } from "react";
 import { RHeading } from "~/lib/atoms/RTypography/RHeading";
-import { AssetsFilters } from "~/routes/discover/components/AssetsFilters/AssetsFilters";
+import { RTabSwitcher } from "~/lib/organisms/RTabSwitcher";
 
-export function ExploreAssets() {
+export function AssetsFilters() {
   const { assets } = useAssetsContext();
 
   return (
     <div className={styles.wrapper}>
-      <RHeading size="h5" weight="medium">Explore Assets</RHeading>
-      <AssetsFilters />
+      <RTabSwitcher
+        ariaLabel="tesdt"
+        tabs={[
+          { id: "1", label: "label 1" },
+          { id: "2", label: "label 2" },
+        ]}
+        activeTabId={"1"}
+        onChange={(b: string) => {}}
+      />
     </div>
   );
 }
