536a34d96359c084b6d3990f9ece932d8fa077d4
AndriyPytsko <AserExtensa52@gmail.com>
2026-07-27 12:32:15 +0300

    MAV Add new assets prices endpoint

--- app/routes/discover/components/Highlights/HighlightCard.tsx
+++ app/routes/discover/components/Highlights/HighlightCard.tsx
@@ -3,10 +3,19 @@ import type { AssetType } from "~/lib/apis/rwa/assets/assets.types";
 import Money from "~/lib/atoms/Money";
 import { Link } from "@remix-run/react";
 import { RText } from "~/lib/atoms/RTypography/RText";
+import { useAssetsContext } from "~/providers/AssetsProvider/assets.provider";
 
 export function HighlightCard(props: { asset: AssetType }) {
   const { asset } = props;
+  const { prices } = useAssetsContext();
 
+  const assetPrices = prices[asset.address] ?? {};
+
+  const priceChange = {
+    amount: assetPrices.change_24h?.delta_abs ?? 0,
+    percentage: assetPrices.change_24h?.change_pct ?? 0,
+  };
+  const isNegative = priceChange?.percentage < 0;
   return (
     //TODO add correct link
     <Link to="/" className={styles.cardWrapper}>
@@ -22,11 +31,18 @@ export function HighlightCard(props: { asset: AssetType }) {
 
       <div className={styles.cardBlock}>
         <RText size="body-sm" weight="medium">
-          $<Money>{asset.stats?.price.usd ?? 0}</Money>
+          $
+          <Money>
+            {assetPrices.usd ??
+              assetPrices.price ??
+              asset.stats?.price.usd ??
+              asset.finance.value_per_token}
+          </Money>
         </RText>
         {/*TODO ask what value use here*/}
         <RText size="body-s" color="green-500">
-          +<Money>{asset.stats?.price.usd ?? 0}</Money>%
+          {isNegative ? "-" : "+"}
+          <Money>{priceChange.percentage ?? 0}</Money>%
         </RText>
       </div>
     </Link>
