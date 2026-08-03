57f9b454354f93bc6cc853861514515703221b1c
AndriyPytsko <AserExtensa52@gmail.com>
2026-08-03 13:28:45 +0300

    MAV Fix logo problem

--- app/routes/trade.$address/route.tsx
+++ app/routes/trade.$address/route.tsx
@@ -20,11 +20,11 @@ export default function TradePage() {
   return (
     <PageLayout includeContainer={false}>
       <Container>
-        <AssetDetails asset={asset} />
-        <div className={styles.contentBlock}>
-          {/*<ChartBlock asset={asset} />*/}
-          <div className={styles.buySellContainer}></div>
-        </div>
+        {/*<AssetDetails asset={asset} />*/}
+        {/*<div className={styles.contentBlock}>*/}
+        {/*  /!*<ChartBlock asset={asset} />*!/*/}
+        {/*  <div className={styles.buySellContainer}></div>*/}
+        {/*</div>*/}
         <AssetTabs asset={asset} />
       </Container>
     </PageLayout>
