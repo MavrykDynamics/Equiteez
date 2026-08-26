6662986efda4e740c4c7e15183bee9db1d1c2bdf
AndriyPytsko <AserExtensa52@gmail.com>
2026-08-26 15:39:21 +0300

    MAV Discover v2 (Add mock images)

---
+++ app/mocks/asset-image-urls.mock.ts
@@ -0,0 +1,17 @@
+import assetImage01 from "~/assets/redesign/asset-images/RAssetImage01.png";
+import assetImage02 from "~/assets/redesign/asset-images/RAssetImage02.png";
+import assetImage03 from "~/assets/redesign/asset-images/RAssetImage03.png";
+import assetImage04 from "~/assets/redesign/asset-images/RAssetImage04.png";
+import assetImage05 from "~/assets/redesign/asset-images/RAssetImage05.png";
+import assetImage06 from "~/assets/redesign/asset-images/RAssetImage06.png";
+import assetImage07 from "~/assets/redesign/asset-images/RAssetImage07.png";
+
+export const ASSET_IMAGE_URLS_BY_ADDRESS: Record<string, string> = {
+  KT1XLUiaPpivxi2e4U1wEctQ7DKDkdz9GQgK: assetImage01,
+  KT1WyJZEfkM623Q4SugEaBCcFQReHYJ22avD: assetImage02,
+  KT1SYFeAjDsu7KXfN3VUgyuzfdMth16r8BQ2: assetImage03,
+  KT1Xd6Q4Gkvdt9avjisJr9cyN8M8aj111ayL: assetImage04,
+  KT1CqhFdiCzBzZKq9jgBmdT9WKueZHp7Qa5b: assetImage05,
+  KT1QNcmMW1p2pcqdke1R4d9tyPyrioY8RU4H: assetImage06,
+  KT1FBJnqyY3EHB8dpwZ51gZqHBAXZifxwDnc: assetImage07,
+};
