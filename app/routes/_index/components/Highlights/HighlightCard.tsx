9e95672b47cfe6b5a8210b76a1c0e7972cb5aa48
AndriyPytsko <AserExtensa52@gmail.com>
2026-08-26 16:03:24 +0300

    MAV Discover v2 (Highlights block)

--- app/routes/_index/components/Highlights/HighlightCard.tsx
+++ app/routes/_index/components/Highlights/HighlightCard.tsx
@@ -1,44 +1,63 @@
-import styles from "./styles.module.css";
-import { AssetHighlightType } from "~/lib/apis/rwa/assets/assets.types";
-import Money from "~/lib/atoms/Money";
 import { generatePath, Link } from "@remix-run/react";
-import { RText } from "~/lib/atoms/RTypography/RText";
+
 import { ROUTES } from "~/consts";
+import type { AssetHighlightType } from "~/lib/apis/rwa/assets/assets.types";
+import Money from "~/lib/atoms/Money";
+import { RText } from "~/lib/atoms/RTypography/RText";
+import { ASSET_IMAGE_URLS_BY_ADDRESS } from "~/mocks/asset-image-urls.mock";
+
+import styles from "./styles.module.css";
+
+const fallbackImageUrl = Object.values(ASSET_IMAGE_URLS_BY_ADDRESS)[0];
 
 export function HighlightCard(props: { asset: AssetHighlightType }) {
   const { asset } = props;
   const price = asset.price.usd;
   const priceChange = asset.change_24h?.pct;
   const isNegative = (priceChange ?? 0) < 0;
+  const imageUrl =
+    ASSET_IMAGE_URLS_BY_ADDRESS[asset.address] ?? fallbackImageUrl;
 
   return (
     <Link
       to={generatePath(ROUTES.trade, { address: asset.address })}
       className={styles.cardWrapper}
     >
-      <div className={styles.cardBlock}>
-        <RText size="body-sm" weight="medium">
-          {asset.name.replace("RWA", "").replace("Token", "")}
-        </RText>
-        <RText size="body-s" color="neutral-600">
-          {asset.symbol.replace("-usdt", "").toUpperCase()}
-        </RText>
-      </div>
+      <img alt="" className={styles.cardImage} src={imageUrl} />
 
-      <div className={styles.cardBlock}>
-        <RText size="body-sm" weight="medium">
-          $<Money fiat>{price}</Money>
-        </RText>
-        {priceChange !== undefined && priceChange !== null ? (
-          <RText size="body-s" color={isNegative ? "red-500" : "green-500"}>
-            {isNegative ? "" : "+"}
-            <Money fiat>{priceChange}</Money>%
+      <div className={styles.cardContent}>
+        <div className={styles.cardBlock}>
+          <RText size="body-sm" weight="medium">
+            {asset.symbol.replace("-usdt", "").toUpperCase()}
           </RText>
-        ) : (
-          <RText size="body-s" color="neutral-600">
-            --
+          <RText className={styles.assetName} size="body-s" color="neutral-700">
+            {asset.name.replace("RWA", "").replace("Token", "")}
           </RText>
-        )}
+        </div>
+
+        <div className={styles.priceBlock}>
+          <RText size="body-sm" weight="medium">
+            $<Money fiat>{price}</Money>
+          </RText>
+          {priceChange !== undefined && priceChange !== null ? (
+            <RText
+              className={styles.priceChange}
+              size="body-s"
+              color={isNegative ? "red-500" : "green-500"}
+            >
+              {isNegative ? "" : "+"}
+              <Money fiat>{priceChange}</Money>%
+            </RText>
+          ) : (
+            <RText
+              className={styles.priceChange}
+              size="body-s"
+              color="neutral-600"
+            >
+              --
+            </RText>
+          )}
+        </div>
       </div>
     </Link>
   );
