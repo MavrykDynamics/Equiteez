b1634c0f4935d5805b54dc3ec6bfedb1b879973b
AndriyPytsko <AserExtensa52@gmail.com>
2026-09-16 12:01:11 +0300

    [MAV-4037] - Refresh user data from notifier events

--- app/providers/UserProvider/user.provider.tsx
+++ app/providers/UserProvider/user.provider.tsx
@@ -35,6 +35,11 @@ import {
   getIsKycedForAddress,
 } from "./helpers/userStatus.helpers";
 import type { UserAccountStatusQuery } from "~/utils/__generated__/graphql";
+import {
+  NotifierChannel,
+  NotifierWalletEvent,
+} from "~/providers/NotificationsProvider/notifications.const";
+import { useNotifierEvent } from "~/providers/NotificationsProvider/hooks/useNotifierEvent";
 
 export const userContext = React.createContext<UserContext>(undefined!);
 
@@ -223,11 +228,13 @@ export const UserProvider = ({ children }: Props) => {
     });
 
     updateUserAccountStatus(data);
-  }, [
-    accountAddress,
-    refetchUserAccountStatusQuery,
-    updateUserAccountStatus,
-  ]);
+  }, [accountAddress, refetchUserAccountStatusQuery, updateUserAccountStatus]);
+
+  useNotifierEvent(
+    NotifierChannel.Wallet,
+    NotifierWalletEvent.KycSetMember,
+    refetchUserAccountStatus
+  );
 
   useEffect(() => {
     updateUserAccountStatus(userAccountStatusData);
